ea447a3ff2f5c6ea5d8b8c2e04210c5c7632a8d6
elmo5676 <elmo5676@gmail.com>
2018-04-26 23:02:43 -0700

    Initial Commit

---
+++ MQF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MQF
+//
+//  Created by elmo on 4/26/18.
+//  Copyright © 2018 elmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
